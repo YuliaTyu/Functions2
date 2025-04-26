4ff3b30e4cfee06834d09f7cde7d0bc11dee353b
Юля <@DESKTOP-LDN2BMM>
2025-04-26 11:31:31 +0300

    StaticLIB DONE

---
+++ LIBCheck/main.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<FillRand.h>
+#include<Print.h>
+#include<Sort.h>
+
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int n = 5;
+	double arr[n];
+	FillRand(arr, n);
+	Print(arr, n);
+	Sort(arr, n);
+	Print(arr, n);
+
+	cout << delimiter << endl;
+
+	double i_arr_2[ROWS][COLS];
+	FillRand(i_arr_2, ROWS, COLS);
+	Print(i_arr_2, ROWS, COLS);
+
+}
---
+++ include/constants.h
@@ -0,0 +1,7 @@
+#pragma once
+#define delimiter "\n------------------------------------\n"
+#define delimiter2 "\n===================================\n"
+
+
+const int ROWS = 4;
+const int COLS = 10;
